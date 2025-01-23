32a27ac510c83911d4335b3c66ad4996c1955753
Yulesh Practice <Yuleshlearningcurve@gmail.com>
2025-01-24 06:49:05 +1100

    add barrier pay function. To do next is: add all week pay calculation function

--- index.js
+++ index.js
@@ -19,9 +19,12 @@ app.get("/", (req, res) =>{
 app.post("/submit", (req, res) =>{
     const inputData = req.body.notes;
     const eachDay = inputData.replace(/\r/g,"").split("\n");
+    if (eachDay[eachDay.length - 1].trim() === ""){
+        eachDay.pop();
+    }
     let arrayOfObjects = timeExtractor(eachDay);
     arrayOfObjects.forEach(object => {
-        console.log(object.original + object.pay);
+        console.log(object.original + " " + object.pay);
     });
 
     res.render("partials/calculated.ejs", {result: arrayOfObjects});
@@ -35,12 +38,45 @@ app.listen(port, () => {
 //a helper function which extracts the start and end time of working hours from the array items\
 function timeExtractor(array){
     return array.map(item => {
-        const [firstHalf, endTime] = item.split("to").map(time => time.trim());
+        const [firstHalf, secondHalf] = item.split("to").map(time => time.trim());
         const startTime = firstHalf.split(" ").slice(-2).join(" ");
-        const pay = payCalculator(startTime, endTime);
+        const endTime = secondHalf.split(" ").slice(0,2).join(" ");
+        let jobType = "";
+        if (secondHalf.split(" ").length === 3){
+            jobType = "barrier";
+        }
+        const pay = payCalculator(startTime, endTime, jobType);
         return {original: item, pay}
     });
 }
+
+function payCalculator(startTime, endTime, jobType){
+    console.log(jobType);
+    const [hourDiff, minDiff] = timeDiffCalculator(startTime, endTime);
+    let totalPay;
+    if (jobType.toLowerCase() === "barrier"){
+        totalPay = hourDiff * 40 + minDiff * 0.667;    
+    }else{
+        totalPay = hourDiff * 30 + minDiff * 0.5;
+    }
+    totalPay = totalPay.toFixed(2);
+    return totalPay;
+}
+
+function timeDiffCalculator(startTime, endTime){    //function to calculate timeDifference between working periods
+    const [startHour, startMin] = timeConverter(startTime);
+    const [endHour, endMin] = timeConverter(endTime);
+    let hourDiff = endHour - startHour;
+    let minDiff = endMin - startMin;
+    
+    if (minDiff < 0){
+        minDiff = minDiff + 60;
+        hourDiff -= 1;
+    }
+
+    return [hourDiff, minDiff];
+}
+
 
 //creating a function to convert the time to 24 hour format
 
@@ -58,26 +94,7 @@ function timeConverter(timeStr){
     return [hours, mins];
 }
 
-function timeDiffCalculator(startTime, endTime){    //function to calculate timeDifference between working periods
-    const [startHour, startMin] = timeConverter(startTime);
-    const [endHour, endMin] = timeConverter(endTime);
-    let hourDiff = endHour - startHour;
-    let minDiff = endMin - startMin;
-    
-    if (minDiff < 0){
-        minDiff = minDiff + 60;
-        hourDiff -= 1;
-    }
 
-    return [hourDiff, minDiff];
-}
 
-function payCalculator(startTime, endTime){
-    const [hourDiff, minDiff] = timeDiffCalculator(startTime, endTime);
-    let totalPay = hourDiff * 30 + minDiff * 0.5;
-    totalPay = totalPay.toFixed(2);
-    console.log(totalPay);
-    return totalPay;
-}
 
 
